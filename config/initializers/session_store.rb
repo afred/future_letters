a353b9c5a3dd3c365feb0d957fa5525e117eacac
Andrew Myers <afredmyers@gmail.com>
2015-02-26 22:34:46 -0500

    Changes app name from 'Future Letters' to 'We Knew'

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_future_letters_session'
+Rails.application.config.session_store :cookie_store, key: '_we_knew'
